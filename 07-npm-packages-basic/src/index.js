f36d171c75d74adb62b79b060bc6cb4a3e765fa9
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-08-28 21:39:17 -0300

    07-npm-packages

---
+++ 07-npm-packages-basic/src/index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(logSymbols.success, chalk.green.italic("servidor iniciado com sucesso"));
+console.log(logSymbols.error, chalk.red.italic("servidor iniciado com sucesso"));
+
