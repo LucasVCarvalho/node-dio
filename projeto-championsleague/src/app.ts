916c9f59a87dd2b4afa140f341b7970680e7ebab
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-08-30 20:22:48 -0300

    cors example

--- projeto-championsleague/src/app.ts
+++ projeto-championsleague/src/app.ts
@@ -7,6 +7,12 @@ function createApp() {
 
     app.use(express.json());
     app.use("/api", router);
+
+    // const corsOptions = {
+    //     origin: ["http://gov.br"],
+    //     methods: ["GET", "UPDATE"],
+    // }
+
     app.use(cors());
     return app;
 }
